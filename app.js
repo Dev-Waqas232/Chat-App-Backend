999009eb1554a7fa104da80ff9bb08fb7304cd65
Waqas Munir <devwaqas232@gmail.com>
2025-04-10 21:13:58 +0500

    Added Database Connection

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./utils/dbConnect.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, connectDB());
