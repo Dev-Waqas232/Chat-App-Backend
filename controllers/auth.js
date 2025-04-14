7fda1a36efa2b008eeb13fb44bc8d2c4f057ae56
Waqas <devwaqas232@gmail.com>
2025-04-14 11:38:40 +0500

    Configured CORS for cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -42,7 +42,7 @@ const login = catchAsync(async (req, res, next) => {
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "Strict",
+    sameSite: "None",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
